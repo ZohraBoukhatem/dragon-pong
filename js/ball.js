c23182d92edae462fe066467e280b9488861294b
Zohra <zohra.boukhatem1@gmail.com>
2023-10-29 12:53:49 +0100

    playing around with clamp function and ball movement

--- js/ball.js
+++ js/ball.js
@@ -1,17 +1,36 @@
 class Ball {
     constructor() {
         this.positionX = 0
-        this.positionY = 0
+        this.positionY = 700
         this.height = 30
         this.width = 30
 
-        const ball = document.getElementById("ball")
-        ball.style.height = this.height + px
-        ball.style.width = this.width + px
-        ball.style.bottom = this.positionY +px
-        ball.style.left = this.positionX + px
+        this.ball = document.getElementById("ball")
+        this.ball.style.height = this.height + "px"
+        this.ball.style.width = this.width + "px"
+        this.ball.style.bottom = this.positionY +"px"
+        this.ball.style.left = this.positionX + "px"
     }
 
-   setInterval() {}
+    move() {
+        if (this.positionY > 0) {
+            this.positionY -= 30
+    } else if (this.positionY = 0) {
+            this.positionY += 30
+        }
 
+
+
+        this.positionX += 50
+    
+
+        this.ball.style.bottom = this.positionY + "px"
+        this.ball.style.left = this.positionX + "px"
+          }
 }
+
+const ball = new Ball()
+
+setInterval(() => {
+    ball.move()
+}, 500)
