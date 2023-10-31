0cff8cd062b1a460aa89f415940ca0864d87d561
Zohra <zohra.boukhatem1@gmail.com>
2023-10-31 15:12:07 +0100

    added wall collision; added increasing speed for ball; added freeze for players

--- js/ball.js
+++ js/ball.js
@@ -1,9 +1,10 @@
 class Ball {
   constructor() {
     this.positionX = 0;
-    this.positionY = 50
+    this.positionY = 500
     this.height = 28;
     this.width = 28;
+    this.speed = 1
 
     this.ball = document.getElementById("ball");
     this.ball.style.height = this.height + "px";
@@ -11,33 +12,56 @@ class Ball {
     this.ball.style.bottom = this.positionY + "px";
     this.ball.style.left = this.positionX + "px";
   }
-
   
+  increaseSpeed() {
+    this.speed++
+  }
+  
+  
+  //X POSITION
   moveRight() {
-    this.positionX++ 
+    this.positionX += this.speed 
     this.ball.style.left = this.positionX + "px";
-  if (ball.positionX == 1035) {
-   location.href = "./gameover1.html"; }
-    }
-
-  moveLeft() {
-    this.positionX--
-    this.ball.style.left = this.positionX + "px"
-    
-  if (ball.positionX == 0) {
-  location.href = "./gameover2.html"; }
+    if (ball.positionX >= 1035) {
+    location.href = "./gameover1.html"; }
   }
 
-  checkCollision(player) {
-    
+  moveLeft() {
+    this.positionX -= this.speed
+    this.ball.style.left = this.positionX + "px"
+    if (ball.positionX <= 0) {
+    location.href = "./gameover2.html"; }
+  }
+
+  checkCollision(player) {  
     if (
       player.positionX < ball.positionX + ball.width &&
       player.positionX + player.width > ball.positionX &&
       player.positionY < ball.positionY + ball.height &&
       player.positionY + player.height > ball.positionY
-  ) {
+    ) {
     return true
+    }
   }
 
+  //Y POSITION
+  moveUp() {
+    this.positionY ++
+    this.ball.style.bottom = this.positionY + "px"
   }
+  moveDown() {
+    this.positionY--
+    this.ball.style.bottom = this.positionY + "px"
+  }
+
+  checkCollisionWithWall(wall) {
+    if (
+      ball.positionY == wall
+    ) {
+      return true
+    }
+  }
+
+
+
 }
--- js/main.js
+++ js/main.js
@@ -2,16 +2,17 @@
 const player1 = new Player1();
 const player2 = new Player2();
 const ball = new Ball();
+let player1CanMove = false
 
 document.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "ArrowDown":
-      if (player2.positionY > 0) {
+      if (player2.positionY > 0 && !player1CanMove) {
       player2.moveDown();
       }
       break;
     case "ArrowUp":
-      if (player2.positionY < 350) {
+      if (player2.positionY < 350 && !player1CanMove) {
       player2.moveUp();
       }
       break;
@@ -22,16 +23,29 @@ document.addEventListener("keydown", (event) => {
 document.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "s":
-      if (player1.positionY > 0) {
+      if (player1.positionY > 0 && player1CanMove) {
       player1.moveDown(); }
       break;
     case "w":
-      if (player1.positionY < 350) {
+      if (player1.positionY < 350 && player1CanMove) {
       player1.moveUp(); 
     }
       break;
   }
 });
+
+function freezePlayer(playerNumber) {
+  if(playerNumber == 2) {
+    player1CanMove = true 
+  } else if (playerNumber == 1) {
+    player1CanMove = false
+  }
+}
+
+
+const intervalID = setInterval(() => {
+  ball.increaseSpeed()
+},5000);
 
 
 function movingRight() {
@@ -40,6 +54,7 @@ const intervalID = setInterval(() => {
    if (ball.checkCollision(player2)) {
     clearInterval(intervalID)
     movingLeft() 
+    freezePlayer(2)
   }
   }, 5);
 }
@@ -50,9 +65,35 @@ const intervalID = setInterval(() => {
   if (ball.checkCollision(player1)) {
     clearInterval(intervalID)
     movingRight()
+    freezePlayer(1)
   }
 }, 5);
 }
 
+function movingDown() {
+  const intervalID = setInterval(() => {
+    const wall = 0
+    ball.moveDown();
+   if (ball.checkCollisionWithWall(wall)) {
+    clearInterval(intervalID)
+    movingUp() 
+  }
+  }, 5);
+}
+
+function movingUp() {
+  const intervalID = setInterval(() => {
+    const wall = 500
+    ball.moveUp();
+   if (ball.checkCollisionWithWall(wall)) {
+    clearInterval(intervalID)
+    movingDown() 
+  }
+  }, 5);
+}
+
 
 movingRight()
+
+movingDown()
+
